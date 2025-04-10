5f9eeaf0c05083670244b41b92473a59bb5bd976
victimcoom <1452473649@qq.com>
2025-04-10 19:48:28 +0800

    Revert "refactor: 重构了登录和注册界面 (#2)"
    This reverts commit bd6f85e6e91367b2f892d38984646f5e294a93e7.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
+    <nav>
+      <router-link to="/login">登录</router-link> |
+      <router-link to="/register">注册</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <style>
 #app {
-  font-family: Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  margin: 0;
-  padding: 0;
 }
 
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+nav {
+  padding: 30px;
 }
 
-body {
-  background-color: #f5f5f5;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  margin: 0 10px;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Register from '../components/Register.vue'
+import RegisterPage from '../components/RegisterPage.vue'
+import LoginPage from '../components/LoginPage.vue'
+import ProfilePage from '../components/ProfilePage.vue'
 
 const routes = [
   {
-    path: '/',
-    redirect: '/register'
-  },
-  {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: RegisterPage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfilePage
+  },
+  {
+    path: '/',
+    redirect: '/login'
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// 添加路由守卫
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/profile' && !token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
