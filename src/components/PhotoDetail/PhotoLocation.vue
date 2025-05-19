2edb4119a13ab6f784c910965b61ee432591297d
Seeridia <Seeridia@gmail.com>
2025-05-19 23:37:59 +0800

    Revert "fix: 采用火星坐标系"
    This reverts commit ab63c5167bddbf05e6eb6b2a4620855b59987485.

--- src/components/PhotoDetail/PhotoLocation.vue
+++ src/components/PhotoDetail/PhotoLocation.vue
@@ -2,33 +2,26 @@
   PhotoLocation.vue - 照片详情/位置信息组件
 -->
 
-<!--
-  使用了 wgs84_to_gcj02.js 工具函数来转换WGS-84坐标到GCJ-02坐标
-
-  图片的位置信息通常是 WGS-84 坐标系，GCJ-02 坐标系是中国大陆地区常用的坐标系。（在后续接高德的时候也会使用 GCJ-02）
--->
-
 <template>
-  <div class="info-group" v-if="hasLocation">
-    <h3 class="info-group-title">位置信息</h3>
-    <div class="info-item">
-      <span class="info-label">经度</span>
-      <span class="info-value">{{ formatCoordinate(gcjCoordinates.longitude, 'lng') }}</span>
+    <div class="info-group" v-if="hasLocation">
+        <h3 class="info-group-title">位置信息</h3>
+        <div class="info-item">
+            <span class="info-label">经度</span>
+            <span class="info-value">{{ formatCoordinate(photo.location.longitude, 'lng') }}</span>
+        </div>
+        <div class="info-item">
+            <span class="info-label">纬度</span>
+            <span class="info-value">{{ formatCoordinate(photo.location.latitude, 'lat') }}</span>
+        </div>
+        <!-- TODO：之后考虑添加地图组件或是来个接口显示位置名称 -->
+        <SfLinkButton icon="fas fa-map-marker-alt" @click="openMap">
+            在地图中查看
+        </SfLinkButton>
     </div>
-    <div class="info-item">
-      <span class="info-label">纬度</span>
-      <span class="info-value">{{ formatCoordinate(gcjCoordinates.latitude, 'lat') }}</span>
-    </div>
-    <!-- TODO：之后考虑添加地图组件或是来个接口显示位置名称 -->
-    <SfLinkButton icon="fas fa-map-marker-alt" @click="openMap">
-      在地图中查看
-    </SfLinkButton>
-  </div>
 </template>
 
 <script>
 import { SfLinkButton } from '../ui';
-import { wgs84_to_gcj02 } from '../../utils/wgs84_to_gcj02.js';
 
 export default {
   name: 'PhotoLocation',
@@ -44,20 +37,6 @@ export default {
   computed: {
     hasLocation() {
       return !!(this.photo?.location?.latitude && this.photo?.location?.longitude)
-    },
-    // 添加转换后的GCJ-02坐标计算属性
-    gcjCoordinates() {
-      if (!this.hasLocation) return { latitude: 0, longitude: 0 };
-
-      const [gcjLng, gcjLat] = wgs84_to_gcj02(
-        this.photo.location.longitude,
-        this.photo.location.latitude
-      );
-
-      return {
-        longitude: gcjLng,
-        latitude: gcjLat
-      };
     }
   },
   methods: {
@@ -79,8 +58,8 @@ export default {
     openMap() {
       if (!this.hasLocation) return;
 
-      // 使用计算好的火星坐标(GCJ-02)打开地图
-      const mapUrl = `https://maps.apple.com/?q=${this.gcjCoordinates.latitude},${this.gcjCoordinates.longitude}`;
+      // 使用系统地图应用打开位置
+      const mapUrl = `https://maps.apple.com/?q=${this.photo.location.latitude},${this.photo.location.longitude}`;
       window.open(mapUrl, '_blank');
     }
   }
